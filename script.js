90e98cff9210a58467cdf423287c03690f11b46a
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2023-05-01 17:32:55 +0100

    poster added

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const info = document.getElementById("info");
 const castCrew = document.getElementById("castcrew");
 const home = document.getElementById("home");
 
+const mainHome = document.getElementsByClassName("main-home")[0];
+
 window.onload = ()=>{
   home.style.borderBottom = "2px solid rgb(149, 149, 149)";
 }
+
 home.addEventListener("click", ()=>{
   home.style.borderBottom = "2px solid rgb(149, 149, 149)";
   castCrew.style.removeProperty("border-bottom");
   info.style.removeProperty("border-bottom");
+
+  mainHome.style.display = "flex";
   console.log("home click");
 })
 
@@ -16,9 +21,15 @@ info.addEventListener("click", ()=>{
   info.style.borderBottom = "2px solid rgb(149, 149, 149)";
   castCrew.style.removeProperty("border-bottom");
   home.style.removeProperty("border-bottom");
+
+  mainHome.style.display = "none";
   console.log("info click");
-  console.log(home.style.borderBottom);
+
 })
+
+// mainHome.addEventListener("click", ()=>{
+//   console.log("mainhome");
+// })
 
 castCrew.addEventListener("click", ()=>{
   castCrew.style.borderBottom = "2px solid rgb(149, 149, 149)";
