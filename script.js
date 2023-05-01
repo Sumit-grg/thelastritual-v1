281d1f79629aa639d62635610c5e55d3a261b501
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2023-05-01 16:24:13 +0100

    nav links

---
+++ script.js
@@ -0,0 +1,11 @@
+const info = document.getElementById("info");
+const castCrew = document.getElementById("castcrew");
+
+
+info.addEventListener("click", ()=>{
+
+})
+
+castCrew.addEventListener("click", ()=>{
+
+})
