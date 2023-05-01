555dfd3bf6f1c5e69e25491d3f49d4505e9d7873
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2023-05-01 16:56:54 +0100

    text-deco nav

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const info = document.getElementById("info");
 const castCrew = document.getElementById("castcrew");
+const home = document.getElementById("home");
 
+window.onload = ()=>{
+  home.style.borderBottom = "2px solid rgb(149, 149, 149)";
+}
+home.addEventListener("click", ()=>{
+  home.style.borderBottom = "2px solid rgb(149, 149, 149)";
+  castCrew.style.removeProperty("border-bottom");
+  info.style.removeProperty("border-bottom");
+  console.log("home click");
+})
 
 info.addEventListener("click", ()=>{
-
+  info.style.borderBottom = "2px solid rgb(149, 149, 149)";
+  castCrew.style.removeProperty("border-bottom");
+  home.style.removeProperty("border-bottom");
+  console.log("info click");
+  console.log(home.style.borderBottom);
 })
 
 castCrew.addEventListener("click", ()=>{
-
+  castCrew.style.borderBottom = "2px solid rgb(149, 149, 149)";
+  info.style.removeProperty("border-bottom");
+  home.style.removeProperty("border-bottom");
+  console.log("castcrew click");
 })
