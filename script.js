5e923ec125fda97dbff97564170de53f402abe2b
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2023-05-03 20:38:33 +0100

    poster algined

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const castCrew = document.getElementById("castcrew");
 const home = document.getElementById("home");
 
 const mainHome = document.getElementsByClassName("main-home")[0];
+const mainInfo = document.getElementsByClassName("main-info")[0];
+const mainCC = document.getElementsByClassName("main-cc")[0];
 
 window.onload = ()=>{
   home.style.borderBottom = "2px solid rgb(149, 149, 149)";
@@ -14,6 +16,8 @@ home.addEventListener("click", ()=>{
   info.style.removeProperty("border-bottom");
 
   mainHome.style.display = "flex";
+  mainInfo.style.display = "none";
+  mainCC.style.display = "none";
   console.log("home click");
 })
 
@@ -23,17 +27,20 @@ info.addEventListener("click", ()=>{
   home.style.removeProperty("border-bottom");
 
   mainHome.style.display = "none";
+  mainInfo.style.display = "flex";
+  mainCC.style.display ="none";
   console.log("info click");
 
 })
-
-// mainHome.addEventListener("click", ()=>{
-//   console.log("mainhome");
-// })
 
 castCrew.addEventListener("click", ()=>{
   castCrew.style.borderBottom = "2px solid rgb(149, 149, 149)";
   info.style.removeProperty("border-bottom");
   home.style.removeProperty("border-bottom");
+
+  mainHome.style.display = "none";
+  mainInfo.style.display = "none";
+  mainCC.style.display ="none";
+
   console.log("castcrew click");
 })
